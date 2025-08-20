5cecb007e2a53f965016dd14d5df715c14cfb307
Patrick Kang <1489148+patrickkang@users.noreply.github.com>
2025-08-19 23:40:20 -0400

    change the default port

--- examples/example-express-mcp/src/config.ts
+++ examples/example-express-mcp/src/config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 export const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN as string;
-export const PORT = parseInt(process.env.PORT ?? "3000", 10);
+export const PORT = parseInt(process.env.PORT ?? "3001", 10);
 export const MCP_SERVER_URL =
   process.env.MCP_SERVER_URL ?? `http://localhost:${PORT}`;
 export const AUDIENCE = process.env.AUTH0_AUDIENCE ?? MCP_SERVER_URL;
