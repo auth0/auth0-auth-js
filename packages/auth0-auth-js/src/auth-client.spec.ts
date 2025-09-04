635797d27ea48ee31dd2462e8b1260ee1d3edb19
guabu <135956181+guabu@users.noreply.github.com>
2025-09-04 08:45:03 +0200

    feat(auth0-auth-js): make `requestExpiry` and `authorizationDetails` params explicit

--- packages/auth0-auth-js/src/auth-client.spec.ts
+++ packages/auth0-auth-js/src/auth-client.spec.ts
@@ -604,6 +604,66 @@ test('backchannelAuthentication - should support RAR', async () => {
   expect(response.authorizationDetails?.[0]!.type).toBe('accepted');
 });
 
+test('backchannelAuthentication - should forward the authorizationDetails parameter', async () => {
+  const authClient = new AuthClient({
+    domain,
+    clientId: '<client_id>',
+    clientSecret: '<client_secret>',
+    authorizationParams: {
+      audience: '<audience>',
+    },
+  });
+
+  const response = await authClient.backchannelAuthentication({
+    bindingMessage: '<binding_message>',
+    loginHint: { sub: '<sub>' },
+    authorizationDetails: [
+      {
+        type: 'accepted',
+      },
+    ],
+  });
+
+  // When we send authorization_details, we should get it back in the response
+  expect(response.authorizationDetails?.[0]!.type).toBe('accepted');
+});
+
+test('backchannelAuthentication - should forward the requestExpiry parameter', async () => {
+  const authClient = new AuthClient({
+    domain,
+    clientId: '<client_id>',
+    clientSecret: '<client_secret>',
+    authorizationParams: {
+      audience: '<audience>',
+    },
+  });
+
+  // intercept the request to the backchannel authentication endpoint to verify the request_expiry parameter
+  let requestExpiry: string | null = null;
+  server.use(
+    http.post(
+      mockOpenIdConfiguration.backchannel_authentication_endpoint,
+      async ({ request }) => {
+        const info = await request.formData();
+        requestExpiry = info.get('request_expiry') as string;
+        return HttpResponse.json({
+          auth_req_id: 'auth_req_123',
+          interval: 0.5,
+          expires_in: 60,
+        });
+      }
+    )
+  );
+
+  await authClient.backchannelAuthentication({
+    bindingMessage: '<binding_message>',
+    loginHint: { sub: '<sub>' },
+    requestExpiry: 180,
+  });
+
+  expect(requestExpiry).toBe('180');
+});
+
 test('backchannelAuthentication - should throw an error when bc-authorize failed', async () => {
   const authClient = new AuthClient({
     domain,
