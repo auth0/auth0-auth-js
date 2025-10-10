21d3cf0bcaede059eba1bb488474d7a8bafad327
Frederik Prijck <frederik.prijck@okta.com>
2025-10-10 12:28:51 +0200

    Remove adapters

--- packages/auth0-fetch-with-auth/src/index.ts
+++ packages/auth0-fetch-with-auth/src/index.ts
@@ -1,11 +1,6 @@
 // Main fetcher class
 export { Fetcher } from './fetcher.js';
 
-// DPoP support
-export { Dpop } from './dpop/dpop.js';
-export { IndexedDBAdapter } from './dpop/adapters/indexeddb-adapter.js';
-export { MemoryAdapter } from './dpop/adapters/memory-adapter.js';
-
 // Types
 export type {
   ResponseHeaders,
@@ -15,7 +10,6 @@ export type {
   AccessTokenFactory,
   FetcherConfig,
 } from './types.js';
-export type { StorageAdapter } from './dpop/adapters/storage-adapter.js';
 
 // Errors
 export { UseDpopNonceError } from './errors.js';
