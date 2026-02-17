714469233badd078031fb1bdbf31ee3523c650be
Frederik Prijck <frederik.prijck@okta.com>
2026-02-17 13:55:07 +0100

    fix(auth0-server-js): ignore invalid jwe as if there is no session (#132)

--- packages/auth0-server-js/src/store/abstract-store.ts
+++ packages/auth0-server-js/src/store/abstract-store.ts
@@ -1,6 +1,18 @@
 import type { EncryptedStoreOptions, AbstractDataStore } from '../types.js';
 import { encrypt, decrypt } from '../encryption/index.js';
-import { JWTPayload } from 'jose';
+import { JWTPayload, errors } from 'jose';
+
+/**
+ * Type guard to determine if an error should be ignored as a session expiration or invalid session, based on the type of error thrown during decryption.
+ * We want to ignore decryption failures and invalid JWE format errors, as these likely indicate an expired or tampered session. However, we do NOT want to ignore claim validation errors (like expiration), as these indicate a valid token that has simply expired.
+ * @param e - The error thrown during decryption
+ * @returns True if the error should be ignored (session expiration/invalid), false otherwise
+ */
+function shouldIgnoreSession(e: unknown): e is Error {
+  // Only catch encryption-related errors (decryption failures, invalid JWE format)
+  // Do NOT catch claim validation errors (expiration, etc.)
+  return e instanceof errors.JWEDecryptionFailed || e instanceof errors.JWEInvalid;
+}
 
 /**
  * Abstract class that can be used to implement an Encrypted JWT State Store, using the 'A256CBC-HS512' encryption algorithm.
@@ -22,6 +34,15 @@ export abstract class AbstractStore<TData extends JWTPayload, TStoreOptions = un
   }
 
   protected async decrypt<TData>(identifier: string, encryptedStateData: string) {
-    return await decrypt(encryptedStateData, this.options.secret, identifier) as TData;
+    try {
+      return (await decrypt(encryptedStateData, this.options.secret, identifier)) as TData;
+    } catch (e: unknown){
+      // When the error indicates a decryption failure, we want to ignore it and return undefined, as this likely means the session has expired or the data is invalid.
+      if (shouldIgnoreSession(e)) {
+        return;
+      }
+
+      throw e;
+    }
   }
 }
