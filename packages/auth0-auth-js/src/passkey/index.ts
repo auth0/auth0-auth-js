b340b42df76a61df080270c85af5bb1cfbba7a60
nandan-bhat <167290944+nandan-bhat@users.noreply.github.com>
2026-05-28 23:04:25 +0530

    feat(auth0-auth-js): add passkeys API support (#177)
    * review: addressing the review comments
    
    * cleanup: removed un-used property
    
    * update: moved 'userMetadata' outside of 'userProfile'
    
    * review: addressing review feedbacks

---
+++ packages/auth0-auth-js/src/passkey/index.ts
@@ -0,0 +1,12 @@
+export { PasskeyClient } from './passkey-client.js';
+export * from './errors.js';
+export type {
+  PasskeySignupChallengeOptions,
+  PasskeySignupChallengeResponse,
+  PasskeyLoginChallengeOptions,
+  PasskeyLoginChallengeResponse,
+  PasskeyCredentialResponse,
+  GetTokenByPasskeyOptions,
+  PasskeyCreationOptions,
+  PasskeyRequestOptions,
+} from './types.js';
